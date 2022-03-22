1125bf7c4b4ce62f96c64fabb6dab2f97e3e8a29
bencfrench <101435989+bencfrench@users.noreply.github.com>
2022-03-22 15:34:04 -0700

    Summary
    Python Basics 1

---
+++ Python Basics Part 1/numbers.py
@@ -0,0 +1,14 @@
+# int and float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+print(2.1 + 4.9)
+print(2 ** 4)
+print(10 // 4)
+print(10 % 4)
+
+# augmented assignment operator
+some_value = 5
+some_value += 2 #or -=, *=, /=
+print(some_value)
